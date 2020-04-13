389b0ef3f783e44634980938a1b3d2c808ee804e
NadinLo <nadin.loewe@posteo.de>
2020-04-13 18:00:52 +0200

    print current choice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,7 +22,8 @@ public class Main {
         }
         //todo: Auswahl Menü anzeigen
         printMenu();
-        //Menüwahl und Zutaten zufügen
+
+        //Menüwahl und Zutaten zufügen/entfernen
         int menuNo = 0;
         int id = 0;
         decision = null;
@@ -43,15 +44,23 @@ public class Main {
                     "the number with a \"-\". When you're finished enter \"0\".");
             while (ingredient != 0) {
                 ingredient = scanner.nextInt();
-                addIngredients.add(id, ingredient);
+                if (ingredient != 0) {
+                    addIngredients.add(ingredient);
+                }
             }
-            addIngredientToMenu(addIngredients);
+            addIngredientToMenu(id, addIngredients);
+        }
+        System.out.println("Would you like to change the amount of this menu? (Y/N)");
+        decision = scanner.next();
+        if (decision.equalsIgnoreCase("Y")) {
+            printEditedMenu(id);
         }
 
-//todo: Zutaten ändern, weitere Menüs wählen bzw. Menenangaben machen
+//todo: weitere Menüs wählen bzw. Menenangaben machen
         //todo: Kundendaten eingeben
     }
-    private static int startNewOrder () {
+
+    private static int startNewOrder() {
         Connection conn = null;
         int orderNo = 0;
         try {
@@ -62,7 +71,7 @@ public class Main {
             int ok = stmt.executeUpdate(command);
             String query = "SELECT Last_INSERT_ID()";
             ResultSet rs = stmt.executeQuery(query);
-            while (rs.next()){
+            while (rs.next()) {
                 orderNo = rs.getInt("Last_INSERT_ID()");
                 System.out.println(orderNo);
             }
@@ -93,7 +102,7 @@ public class Main {
         try {
             String url = "jdbc:mysql://localhost:3306/lieferservice_gastro?user=root";
             conn = DriverManager.getConnection(url);
-            String queryType = "SELECT `id`, `name` FROM `menü_gruppe`";
+            String queryType = "SELECT `id`, `name` FROM `menu_gruppe`";
             Statement stmtType = conn.createStatement();
             ResultSet rsType = stmtType.executeQuery(queryType);
             while (rsType.next()) {
@@ -112,7 +121,7 @@ public class Main {
         try {
             String url = "jdbc:mysql://localhost:3306/lieferservice_gastro?user=root";
             conn = DriverManager.getConnection(url);
-            String queryMenu = "SELECT `menü_nr.`, `name`, `preis` FROM `menü` WHERE `menü_nr.` =" + typeId;
+            String queryMenu = "SELECT `menu_nr`, `name`, `preis` FROM `menu` WHERE `menu_nr` =" + typeId;
             Statement stmtMenu = conn.createStatement();
             ResultSet rsMenu = stmtMenu.executeQuery(queryMenu);
             //todo: springt nicht in die while-Schleife. Keine Fehlermeldung. Setzt Programm in Methode printType() fort
@@ -135,7 +144,7 @@ public class Main {
             conn = DriverManager.getConnection(url);
             String queryIngr = "SELECT zutaten.name, zutaten.vegetarisch " +
                     "FROM `zutatenmix` " +
-                    "Inner JOIN zutaten ON zutatenmix.zutaten_id = zutaten.id WHERE `menü_id` = " + menuNo;
+                    "Inner JOIN zutaten ON zutatenmix.zutaten_id = zutaten.id WHERE `menu_id` = " + menuNo;
             Statement stmtIngr = conn.createStatement();
             ResultSet rsIngr = stmtIngr.executeQuery(queryIngr);
             ArrayList<Integer> vegetarian = new ArrayList<>();
@@ -154,29 +163,34 @@ public class Main {
         }
     }
 
-    private static int chooseMenu (int orderNo, int menuNo) {
+    private static int chooseMenu(int orderNo, int menuNo) {
         Connection conn = null;
         int id = 0;
+        int ok = 0;
 
         try {
             String url = "jdbc:mysql://localhost:3306/lieferservice_gastro?user=root";
             conn = DriverManager.getConnection(url);
-            Statement stmt = conn.createStatement();
-            String command = "INSERT INTO `menü_auswahl`(`bestell_nr.`, menü_nr`) " +
-                    "VALUES (" + orderNo + ", " + menuNo + ")";
-            stmt.executeUpdate(command);
+            Statement stmt = null;
+            String command = "INSERT INTO `menu_auswahl`(`bestell_nr`, `menu_nr`, anzahl) " +
+                    "VALUES (" + orderNo + ", " + menuNo + ", 1)";
+
+            stmt = conn.createStatement();
+            ok = stmt.executeUpdate(command);
             String query = "SELECT Last_INSERT_ID()";
             ResultSet rs = stmt.executeQuery(query);
-            while (rs.next()){
+            while (rs.next()) {
                 id = rs.getInt("Last_INSERT_ID()");
             }
+
+
         } catch (SQLException ex) {
             throw new Error("Problem", ex);
         }
         return id;
     }
 
-    private static void printIngredientList () {
+    private static void printIngredientList() {
         Connection conn = null;
         try {
             String url = "jdbc:mysql://localhost:3306/lieferservice_gastro?user=root";
@@ -188,11 +202,11 @@ public class Main {
                     "ALL INGREDIENTS-------------------------\n" +
                             "id\t| name\t\t\t | is veggi | price\n" +
                             "----------------------------------------\n");
-            while (rs.next()){
+            while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 name = name.concat("               ");
-                name = name.substring(0,15);
+                name = name.substring(0, 15);
                 boolean vegetarian = rs.getBoolean("vegetarisch");
                 double price = rs.getDouble("preis");
                 System.out.println(id + "\t| " + name + "| " + vegetarian + "  \t| " + price);
@@ -212,15 +226,15 @@ public class Main {
         }
     }
 
-    private static void addIngredientToMenu (int id, ArrayList<Integer> ingredients) {
+    private static void addIngredientToMenu(int id, ArrayList<Integer> ingredients) {
         Connection conn = null;
         try {
             String url = "jdbc:mysql://localhost:3306/lieferservice_gastro?user=root";
             conn = DriverManager.getConnection(url);
             String command = "";
-            for (int ingredient: ingredients) {
+            for (int ingredient : ingredients) {
                 if (ingredient > 0) {
-                    command = "INSERT INTO `auswahl_details`(`id`, `zutaten_zufügen`)" +
+                    command = "INSERT INTO `auswahl_details`(`id`, `zutaten_hinzuf`)" +
                             " VALUES (" + id + "," + ingredient + ")";
                 }
                 if (ingredient < 0) {
@@ -231,8 +245,8 @@ public class Main {
                 Statement stmt = conn.createStatement();
                 stmt.executeUpdate(command);
             }
-        } catch (SQLException ex){
-            throw new Error ("Problem", ex);
+        } catch (SQLException ex) {
+            throw new Error("Problem", ex);
         } finally {
             try {
                 if (conn != null) {
@@ -243,6 +257,73 @@ public class Main {
             }
         }
 
+    }
+
+    private static void printEditedMenu(int id) {
+        Connection conn = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/lieferservice_gastro?user=root";
+            conn = DriverManager.getConnection(url);
+            Statement stmt = null;
+            System.out.println("your chosen menu: ");
+            String query = "SELECT menu_auswahl.menu_nr, menu.name, menu.preis " +
+                    "FROM `menu_auswahl` " +
+                    "INNER JOIN menu ON menu_auswahl.menu_nr = menu.menu_nr " +
+                    "WHERE menu_auswahl.auswahl_detail_id = " + id;
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()){
+                int menuNo = rs.getInt("menu_auswahl.menu_nr");
+                String menuName = rs.getString("menu.name");
+                double price = rs.getDouble("menu.preis");
+                System.out.println(menuNo + ") " + menuName + ": " + df.format(price) + " €");
+
+                query = "SELECT zutaten.name " +
+                        "FROM zutatenmix " +
+                        "INNER JOIN zutaten ON zutatenmix.zutaten_id = zutaten.id WHERE zutatenmix.menü_id = " + menuNo;
+                rs = stmt.executeQuery(query);
+                while (rs.next()){
+                    String ingredientName = rs.getString("zutaten.name");
+                    System.out.print(ingredientName + ", ");
+                }
+                System.out.println();
+            }
+            query = "SELECT zutaten.name, zutaten.preis " +
+                    "FROM `auswahl_details` " +
+                    "INNER JOIN zutaten ON auswahl_details.zutaten_hinzuf = zutaten.id " +
+                    "WHERE auswahl_details.id = " + id + " AND auswahl_details.zutaten_hinzuf != 'null'";
+            rs = stmt.executeQuery(query);
+            System.out.println("You want to add following ingredients:");
+            while (rs.next()){
+                String addedIngredient = rs.getString("zutaten.name");
+                double ingrPrice = rs.getDouble("zutaten.preis");
+                System.out.println(addedIngredient + " + " + df.format(ingrPrice) + " €");
+            }
+            query = "SELECT zutaten.name, zutaten.preis " +
+                    "FROM `auswahl_details` " +
+                    "INNER JOIN zutaten ON auswahl_details.zutaten_entfernen = zutaten.id " +
+                    "WHERE auswahl_details.id = " + id + " AND auswahl_details.zutaten_entfernen != 'null'";
+            rs = stmt.executeQuery(query);
+            System.out.println("You don't want to take off following ingredients from your menu:");
+            while (rs.next()){
+                String deletedIngredient = rs.getString("zutaten.name");
+                double ingrPrice = rs.getDouble("zutaten.preis");
+                System.out.println(deletedIngredient + " - " + df.format(ingrPrice) + " €");
+            }
+
+        } catch (SQLException ex) {
+            throw new Error("Problem", ex);
+
+
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
 
