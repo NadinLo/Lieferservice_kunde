3709c90ada3829d819ed4975e8cd0ba7350c8e77
NadinLo <nadin.loewe@posteo.de>
2020-04-30 16:40:57 +0200

    add todoes (price calculation and catching wrong entries)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ import com.company.controller.Controller;
 import com.company.model.Order;
 
 public class Main {
+    //todo: calculating price does not work yet (resp. wrong)
+    //todo: catch wrong entries
 
     public static void main(String[] args) {
         Controller controller = new Controller();
